a619f0d4ca620b384fd9b830483e0b5fee233b4f
Sourav Behra <109072070+soube01@users.noreply.github.com>
2023-12-01 19:28:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// document.addEventListener('DOMContentLoaded', function () {
+//     const video = document.getElementById('background-video');
+//     const videoSources = ['boat.mp4', 'sea.mp4', 'lake.mp4', 'dawn.mp4'];
+//     let currentSourceIndex = 0;
+
+//     function changeVideoSource() {
+//         currentSourceIndex = (currentSourceIndex + 1) % videoSources.length;
+//         video.src = videoSources[currentSourceIndex];
+//     }
+
+//     video.addEventListener('ended', changeVideoSource);
+// });
